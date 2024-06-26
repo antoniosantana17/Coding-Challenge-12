ab56b464d1c33d45ab72c2302957e78d3c7f6729
Antonio Santana <asantana35@usf.edu>
2024-06-25 22:00:03 -0400

    script.js
     "Enhance styling for the data visualization dashboard."

--- script.js
+++ script.js
@@ -26,3 +26,46 @@ d3.csv(csvFilePath).then(data => {
     // Set up the scales
     const xScale = d3.scaleTime().range([0, width]);
     const yScale = d3.scaleLinear().range([height, 0]);
+// Set up the line generator
+const line = d3.line()
+.x(d => xScale(d.date))
+.y(d => yScale(d.value));
+
+// Set up the domains
+xScale.domain(d3.extent(data, d => d.date));
+yScale.domain([0, d3.max(data, d => d.value)]);
+
+// Append the x-axis
+svg.append("g")
+.attr("transform", `translate(0,${height})`)
+.call(d3.axisBottom(xScale));
+
+// Append the y-axis
+svg.append("g")
+.call(d3.axisLeft(yScale));
+
+// Append the line
+svg.append("path")
+.datum(data)
+.attr("class", "line")
+.attr("d", line);
+
+// Add tooltip functionality
+const focus = svg.append("g")
+.attr("class", "focus")
+.style("display", "none");
+
+focus.append("circle")
+.attr("r", 4.5);
+
+focus.append("text")
+.attr("x", 9)
+.attr("dy", ".35em");
+
+svg.append("rect")
+.attr("class", "overlay")
+.attr("width", width)
+.attr("height", height)
+.on("mouseover", () => focus.style("display", null))
+.on("mouseout", () => focus.style("display", "none"))
+.on("mousemove", mousemove);
